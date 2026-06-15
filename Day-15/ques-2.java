4d0515be52169187cd75d5b489987c39fd7c7429
Saksham Agarwal <sakshamagarwal2201@gmail.com>
2026-06-15 21:48:24 +0530

    Create ques-2.java

---
+++ Day-15/ques-2.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Ques2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the size of the array: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.println("Enter the elements of the array:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        // Left rotation by one position
+        int first = arr[0];
+
+        for (int i = 0; i < n - 1; i++) {
+            arr[i] = arr[i + 1];
+        }
+
+        arr[n - 1] = first;
+
+        System.out.println("Array after left rotation:");
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+
+        sc.close();
+    }
+}
