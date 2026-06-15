410cac02f930bbcb119cc6b0357d82a24062c288
Saksham Agarwal <sakshamagarwal2201@gmail.com>
2026-06-15 21:46:15 +0530

    Ques-1.java

--- Day-15/ques-1.java
+++ Day-15/ques-1.java
@@ -1,1 +1,37 @@
+import java.util.Scanner;
 
+public class Ques1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the size of the array: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.println("Enter the elements of the array:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        // Reversing the array
+        int start = 0;
+        int end = n - 1;
+
+        while (start < end) {
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+
+            start++;
+            end--;
+        }
+
+        System.out.println("Reversed Array:");
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+
+        sc.close();
+    }
+}
